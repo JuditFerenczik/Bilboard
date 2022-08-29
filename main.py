16074e2656effc58bc3351093219fcc3e7237226
Judit Ferenczik <jferenczik@gmail.com>
2022-08-29 16:14:52 +0200

    spotipy started

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import requests
 from bs4 import BeautifulSoup
-
+from secretkeys import CLIENT_SECRET,CLIENT_ID
 yearInQuestion = input("Which year do you want to travel to? Type the date in this form YYYY-mm-dd!")
 
 URL = f"https://www.billboard.com/charts/hot-100/{yearInQuestion}/"
@@ -24,3 +24,15 @@ song_list = [[(i+1), all_bands[i], all_songs[i]] for i in range(0,len(all_songs)
 print(song_list)
 #all_songs = [song.getText() for song in all_songs.find(name="span", class_="c-label")]
 #print(all_songs)
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                         client_secret=CLIENT_SECRET))
+searched_word ="artist:"+ song_list[0][1].replace(" ", "%20") + "+track:" + song_list[0][2].replace(" ", "%20")
+#track:Doxy+artist:Miles%20Davis
+#searched_word ="remaster%20track:Doxy+artist:Miles%20Davis"
+print(searched_word)
+results = sp.search(q=searched_word, limit=20)
+print(results)
